34c9277bc5a0cccb8c9a163904c6d196560cf8a4
whitehatvarun <86660633+whitehatvarun@users.noreply.github.com>
2021-11-17 14:42:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+function setup()
+{
+    canvas = createCanvas(280 , 280);
+    canvas.center();
+    background("white");
+    synth = window.speechSynthesis;
+    canvas.mouseReleased(classifyCanvas);
+}
+
+function preload()
+{
+    classifier = ml5.imageClassifier('DoodleNet');
+}
+
+function ClearCanvas()
+{
+    background("white");
+}
+
+function draw()
+{
+    strokeWeight(13);
+    stroke(0);
+    if(mouseIsPressed)
+    {
+        line(pmouseX , pmouseY , mouseX , mouseY);
+    }
+}
+
+function classifyCanvas()
+{
+    classifier.classify(canvas , gotResult);
+}
+
+function gotResult(error , results)
+{
+   if(error)
+   {
+       console.error(error);
+   }
+          
+  console.log(results);
+   document.getElementById("label_object_name").innerHTML = "Label: " + results[0].label;
+   document.getElementById("confidence").innerHTML = "confidence: " + Math.round(results[0].confidence * 100) + "%";
+
+   utterThis = new SpeechSynthesisUtterance(results[0].label);
+   synth.speak(utterThis);
+}
